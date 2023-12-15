928e14dddcc435773f2c760e292303e1b2e6d097
Mario Cuenca <mariocuenca200@gmail.com>
2023-12-15 17:57:54 +0100

    Create theme.py

---
+++ Scripts/Python/OS/theme.py
@@ -0,0 +1,45 @@
+import time
+import os
+from colorama import Fore
+
+os.system("cls")
+print(f"{Fore.RED}AVISO! {Fore.WHITE}Recuerda que al seleccionar un tema el sistema operativo se reiniciará!\n")
+time.sleep(3)
+print("(1) Dark Grey")
+print("(2) Dracula")
+print("(3) Material Darker")
+print("(4) Material Ocean")
+print("(5) Monokai Pro")
+print("(6) Nord")
+print("(7) Nord Wave")
+print("(8) One Dark")
+print("(9) Rosepine\n")
+
+temas = {
+    "1": {"nombre": "Dark Grey", "id": "1"},
+    "2": {"nombre": "Dracula", "id": "2"},
+    "3": {"nombre": "Material Darker", "id": "3"},
+    "4": {"nombre": "Material Ocean", "id": "4"},
+    "5": {"nombre": "Monokai Pro", "id": "5"},
+    "6": {"nombre": "Nord", "id": "6"},
+    "7": {"nombre": "Nord Wave", "id": "7"},
+    "8": {"nombre": "One Dark", "id": "8"},
+    "9": {"nombre": "Rosepine", "id": "9"},
+}
+
+selección = input("Selecciona el tema deseado (1-10): ")
+
+if selección in temas:
+    print(f"El tema seleccionado es: {Fore.YELLOW}{temas[selección]['nombre']}{Fore.WHITE} con ID: {Fore.YELLOW}{temas[selección]['id']}{Fore.WHITE}")
+else:
+    print("Número de tema no válido")
+
+
+seguro = input(f"Tu tema escogido ha sido {Fore.YELLOW}{selección}{Fore.WHITE} estás seguro? (y/n) ")
+
+if seguro.lower == "y" or "yes":
+    print("Reiniciando sistema y aplicando cambios...")
+elif seguro.lower == "n" or "no":
+    print("Saliendo...")
+else:
+    print("Comando no reconocido, saliendo...")
